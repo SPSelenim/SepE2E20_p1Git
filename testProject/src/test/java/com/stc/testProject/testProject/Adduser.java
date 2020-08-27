548b92edbea1bd9830ecb7a643e3cec8e858f197
spselenim <pawal.satish@gmail.com>
2020-08-27 10:10:01 +0530

    Second

---
+++ testProject/src/test/java/com/stc/testProject/testProject/Adduser.java
@@ -0,0 +1,25 @@
+package com.stc.testProject.testProject;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class Adduser extends InvokeApp {
+  @Test
+  public void f() {
+	 init();
+	 try
+	 {
+		 System.out.println("Second Started");
+	 driver.manage().window().maximize();
+	 driver.findElement(By.id("UN")).sendKeys("Helllooooo");
+	 Thread.sleep(2000);
+	 driver.findElement(By.id("PWD")).sendKeys("Helllooooo");
+	 Thread.sleep(2000);
+driver.quit();
+	 }
+	 catch(Exception e)
+	 {
+		 System.out.println(e.getMessage());
+	 }
+  }
+}
