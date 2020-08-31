6c7f2327a8e6cb602a93810d9b5a7a18ea3934ed
spselenim <pawal.satish@gmail.com>
2020-08-31 15:47:28 +0530

    C22

--- testProject/src/test/java/com/stc/testProject/testProject/Adduser.java
+++ testProject/src/test/java/com/stc/testProject/testProject/Adduser.java
@@ -2,7 +2,7 @@ package com.stc.testProject.testProject;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-//Date : 28-08-2020-One- Two-Four -08/31-First-1-100-100C1
+//Date : 28-08-2020-One- Two-Four -08/31-First-1-100-100C1-200-300
 public class Adduser extends InvokeApp {
   @Test
   public void f() {
